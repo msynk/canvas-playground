21b9546918e0a6e0b08b23eec5d3bc1ee2e4b7ab
Saleh Yusefnejad <msynk@outlook.com>
2025-12-18 09:49:44 +0330

    add interactivity

--- interacting/script.js
+++ interacting/script.js
@@ -1,3 +1,37 @@
+const canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const maxRadius = 40;
+const minRadius = 5;
+const mouse = {
+    x: undefined,
+    y: undefined
+};
+
+window.addEventListener('mousemove', function (event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+});
+
+const circles = Array.from({ length: 2000 }, () => {
+    const r = minRadius;
+    const x = Math.random() * (canvas.width - 2 * r) + r;
+    const y = Math.random() * (canvas.height - 2 * r) + r;
+    const dx = (Math.random() - 0.5) * 4;
+    const dy = (Math.random() - 0.5) * 4;
+    return new MovingCircle(x, y, r, dx, dy);
+});
+
+(function animate() {
+    requestAnimationFrame(animate);
+
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+
+    circles.forEach(circle => circle.update(canvas));
+}());
+
+
 function MovingCircle(x, y, r, dx, dy) {
     this.x = x;
     this.y = y;
@@ -11,8 +45,8 @@ function MovingCircle(x, y, r, dx, dy) {
         // ctx.fillStyle = 'blue';
         // ctx.fill();
 
-        ctx.strokeStyle = 'blue';
-        ctx.stroke();
+        ctx.fillStyle = 'blue';
+        ctx.fill();
 
         ctx.closePath();
     };
@@ -29,27 +63,16 @@ function MovingCircle(x, y, r, dx, dy) {
         this.x += this.dx;
         this.y += this.dy;
 
+        // interactivity
+        if (mouse.x - this.x < 50 && mouse.x - this.x > -50
+            && mouse.y - this.y < 50 && mouse.y - this.y > -50) {
+            if (this.r < maxRadius) {
+                this.r += 1;
+            }
+        } else if (this.r > minRadius) {
+            this.r -= 1;
+        }
+
         this.draw(canvas.getContext('2d'));
     };
 }
-
-const canvas = document.querySelector('canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-const circles = Array.from({ length: 100 }, () => {
-    const r = 30;
-    const x = Math.random() * (canvas.width - 2 * r) + r;
-    const y = Math.random() * (canvas.height - 2 * r) + r;
-    const dx = (Math.random() - 0.5) * 4;
-    const dy = (Math.random() - 0.5) * 4;
-    return new MovingCircle(x, y, r, dx, dy);
-});
-
-(function animate() {
-    requestAnimationFrame(animate);
-
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-
-    circles.forEach(circle => circle.update(canvas));
-}());
