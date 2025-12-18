29fd5b2455ab192eb14a20378442679e5e319b2a
Saleh Yusefnejad <msynk@outlook.com>
2025-12-18 09:54:54 +0330

    add colors

--- interacting/script.js
+++ interacting/script.js
@@ -2,6 +2,8 @@ const canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+const interactivityDistance = 50;
+const circleCount = 2000;
 const maxRadius = 40;
 const minRadius = 5;
 const mouse = {
@@ -9,12 +11,14 @@ const mouse = {
     y: undefined
 };
 
+const colorArray = ['#ffaa33', '#99ffaa', '#00ff00', '#4411aa', '#ff1100'];
+
 window.addEventListener('mousemove', function (event) {
     mouse.x = event.x;
     mouse.y = event.y;
 });
 
-const circles = Array.from({ length: 2000 }, () => {
+const circles = Array.from({ length: circleCount }, () => {
     const r = minRadius;
     const x = Math.random() * (canvas.width - 2 * r) + r;
     const y = Math.random() * (canvas.height - 2 * r) + r;
@@ -38,14 +42,12 @@ function MovingCircle(x, y, r, dx, dy) {
     this.r = r;
     this.dx = dx;
     this.dy = dy;
+    this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
 
     this.draw = function (ctx) {
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
-        // ctx.fillStyle = 'blue';
-        // ctx.fill();
-
-        ctx.fillStyle = 'blue';
+        ctx.fillStyle = this.color;
         ctx.fill();
 
         ctx.closePath();
@@ -63,14 +65,13 @@ function MovingCircle(x, y, r, dx, dy) {
         this.x += this.dx;
         this.y += this.dy;
 
-        // interactivity
-        if (mouse.x - this.x < 50 && mouse.x - this.x > -50
-            && mouse.y - this.y < 50 && mouse.y - this.y > -50) {
+        if (Math.abs(mouse.x - this.x) < interactivityDistance &&
+            Math.abs(mouse.y - this.y) < interactivityDistance) {
             if (this.r < maxRadius) {
-                this.r += 1;
+                this.r += 2;
             }
         } else if (this.r > minRadius) {
-            this.r -= 1;
+            this.r -= 2;
         }
 
         this.draw(canvas.getContext('2d'));
